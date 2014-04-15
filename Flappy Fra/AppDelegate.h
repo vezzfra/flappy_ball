21b8be024fe00396b36d7827a7484037cbfbbe4b
Francesco <Francesco@iMac-di-Francesco-2.local>
2014-04-15 18:37:50 +0200

    Initial Commit

---
+++ Flappy Fra/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flappy Fra
+//
+//  Created by Francesco on 15/04/14.
+//  Copyright (c) 2014 Francesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flappy Fra/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Flappy Fra
+//
+//  Created by Francesco on 15/04/14.
+//  Copyright (c) 2014 Francesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
